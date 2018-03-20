ce983cdc4f3fa61eaf2ab1dac30d51b80d2c86d5
simpeD <dtno1.study@gmail.com>
2018-03-20 14:16:01 +0700

    update 20/3

---
+++ Project2_1/Assets/Scripts/Player1/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Use this for initialization
+
+
+    public GameObject smoke;
+    public GameObject gunHead;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);// tao tia di vuong goc voi man hinh
+#if UNITY_IOS || UNITY_ANDROID
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                InSmoke();
+                if (hit.transform.tag.Equals("Target"))
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+#else
+        RaycastHit hit;
+        if (Physics.Raycast(gunHead.transform.position, gunHead.transform.forward, out hit))
+        {
+            InSmoke();
+            if (hit.transform.tag.Equals("Target"))
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+#endif
+
+
+    }
+    void InSmoke()
+    {
+        GameObject sm = Instantiate(smoke, gunHead.transform.position, gunHead.transform.rotation) as GameObject;
+        Destroy(sm, 0.5f);
+    }
+}
---
+++ Project2_1/Assets/Scripts/Player2/P2Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Controller : MonoBehaviour
+{
+    public GameObject parking;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			lookAt(Input.mousePosition);
+		}
+    }
+    public void lookAt(Vector2 mousePosition)
+    {
+        RaycastHit hit = RayFromCamera(mousePosition, 1000.0f);
+        GameObject.Instantiate(parking, hit.point, Quaternion.identity);
+    }
+
+    public RaycastHit RayFromCamera(Vector3 mousePosition, float rayLength)
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        Physics.Raycast(ray, out hit, rayLength);
+        return hit;
+    }
+}
